36cbd7047451fb4ac34c4c60c8e6a7670183cecc
Drew Easter <drewheaster@gmail.com>
2024-07-17 22:02:52 -0500

    adjusted some of the youtube api call and removed api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,41 @@
 
-const apiKey = 'AIzaSyBbMX2clwf2JERc_kk8SbV72U6lsr1Tbg0';
+const apiKey = '';
 const playlistUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLlv-v0mrsq3qJK_95hV7pxBDC_Crw7xUG&key=${apiKey}&maxResults=50`;
 const channelUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=UCT5tNhFbfQRXMkUzZE8XzyQ&key=${apiKey}`;
 
 const handleVideoAppend = function(videos) {
 
     for ( let i = 0; i < videos.length; i++ ) {
+        const videoUrl = videos[i].snippet.resourceId.videoId;
+
+        const youtubeScrollBar = document.getElementById("yt-scrollbar");
+
         const newVideoCard = document.createElement('a');
         const newVideoCardImg = document.createElement('img');
         const newVideoDetails = document.createElement('div');
         const newVideoCardTitle = document.createElement('p');
+        const newVideoTitleDiv = document.createElement('div');
         const newVideoCardChannelImg = document.createElement('img');
     
         newVideoCard.setAttribute("class", "youtube-video-container");
+        newVideoCard.setAttribute("target", "_blank");
+        newVideoCard.setAttribute("href", `https://www.youtube.com/watch?v=${videoUrl}`);
         newVideoCardImg.setAttribute("id", "thumbnail");
         newVideoCardImg.setAttribute("class", "youtube-video-img");
-
+        newVideoCardImg.setAttribute("src", videos[i].snippet.thumbnails.maxres.url);
+        newVideoDetails.setAttribute("class", "youtube-video-details");
+        newVideoCardChannelImg.setAttribute("id", "channel-img");
+        newVideoTitleDiv.setAttribute("class", "youtube-video-title");
         newVideoCardTitle.setAttribute("id", "video-title");
-        newVideoCardChannelImg.setAttribute("class", "youtube-video-img");
-    
-        const videoTitle = document.getElementById('video-title');
-        const videoThumbnail = document.getElementById('thumbnail');
-        const channelProfilePic = document.getElementById('channel-img');
-        const youtubeVideo = document.getElementById('youtube-video');
-        const youtubeScrollBar = document.getElementById("yt-scrollbar");
     
         youtubeScrollBar.appendChild(newVideoCard);
         newVideoCard.appendChild(newVideoCardImg);
+        newVideoCard.appendChild(newVideoDetails);
+        newVideoDetails.appendChild(newVideoCardChannelImg);
+        newVideoDetails.appendChild(newVideoTitleDiv);
+        newVideoTitleDiv.appendChild(newVideoCardTitle);
 
-        const videoUrl = videos[i].snippet.resourceId.videoId;
-        videoTitle.textContent = videos[i].snippet.title;
-        videoThumbnail.setAttribute("src", videos[i].snippet.thumbnails.maxres.url);
-        newVideoCard.setAttribute("href", `https://www.youtube.com/watch?v=${videoUrl}`);
+        newVideoCardTitle.textContent = videos[i].snippet.title;
     }
 
 }
